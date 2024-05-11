6ac1354a6e0f15f0f9438b92b659ac7d3396fac1
Marllon Zucolotto <marlonzucolotto@gmail.com>
2024-05-11 12:44:15 -0300

    Ajustando lógica checkWin + estilo

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,12 +15,47 @@ class _HomePageState extends State<HomePage> {
   final TrayGame trayGame = TrayGame();
 
   //cores
-  final Color black = Colors.black12;
-  final Color white = Colors.white70;
+  final Color black = Colors.black;
+  final Color white = Colors.white;
+  final Color green = Colors.green;
   final Color? grey = Colors.grey[800];
+
+  //começar o primeiro carregamento de tela com as casas limpas
+  String firstLoad(int index) {
+    if (trayGame.cardHouse[index].name == 'empty') {
+      return '';
+    } else {
+      return trayGame.cardHouse[index].name;
+    }
+  }
+
+  void clearHouse() {
+    setState(() {
+      for (var i = 0; i < 9; i++) {
+        trayGame.cardHouse[i] = TypeHouse.empty;
+      }
+      winner = false;
+      gameOver = false;
+    });
+  }
+
+  bool winner = false;
+  bool gameOver = false;
 
   @override
   Widget build(BuildContext context) {
+    //media query
+    final height = MediaQuery.sizeOf(context).height * 0.1;
+    final width = MediaQuery.sizeOf(context).width * 0.8;
+
+    if (trayGame.checkWin) {
+      winner = true;
+    }
+
+    if (!trayGame.cardHouse.contains(TypeHouse.empty) && winner != true) {
+      gameOver = true;
+    }
+
     return MaterialApp(
       home: Scaffold(
         backgroundColor: black,
@@ -29,12 +64,41 @@ class _HomePageState extends State<HomePage> {
           title: Text(
             //titulo
             'JOGO DA VELHA',
-            style: TextStyle(color: white),
+            style: TextStyle(
+              color: white,
+              fontWeight: FontWeight.bold,
+              fontSize: 25,
+            ),
           ),
           backgroundColor: black,
         ),
         body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  'Jogador 1 \nX',
+                  style: TextStyle(
+                      color: white, fontSize: 30, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  'Jogador 2 \nO',
+                  style: TextStyle(
+                      color: white, fontSize: 30, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+            Align(
+              child: Text(
+                'Agora é a vez do ${trayGame.currentTurn == TypeHouse.O ? 'X' : 'O'}',
+                style: TextStyle(
+                    color: white, fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            ),
             GridView.builder(
               // tabuleiro
               shrinkWrap: true,
@@ -45,25 +109,75 @@ class _HomePageState extends State<HomePage> {
               itemBuilder: (BuildContext context, int index) {
                 return GestureDetector(
                   onTap: () {
+                    //OnTap para definir o que acontecera quando clicar
+                    if (winner) {
+                      return;
+                    }
                     if (trayGame.cardHouse[index] == TypeHouse.empty) {
-                    setState(() {
-                      trayGame.markHouse(index);
-                      trayGame.passTurn();
-                    });
+                      setState(() {
+                        trayGame.passTurn();
+                        trayGame.markHouse(index);
+                      });
                     }
                   },
                   child: Card(
-                    color: grey,
+                    color: trayGame.winColor.contains(index.toString()) &&
+                            winner == true
+                        ? green
+                        : grey,
                     child: Center(
                       child: Text(
-                        trayGame.cardHouse[index].name,
-                        style: TextStyle(color: white),
+                        firstLoad(index),
+                        style: TextStyle(color: white, fontSize: 50),
                       ),
                     ),
                   ),
                 );
               },
             ),
+            const SizedBox(height: 10), //espaçamento
+            if (winner)
+              Text(
+                "O ganhador é ${trayGame.currentTurn == TypeHouse.X ? 'X' : 'O'}",
+                style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              )
+            else if (gameOver)
+              const Text(
+                "Ninguém ganhou!!",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              )
+            else
+              const Text('',
+                  style: TextStyle(color: Colors.transparent, fontSize: 30)),
+            Container(
+              //botão para apagar os itens na tela
+              color: Colors.transparent,
+              height: height,
+              width: width,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton.icon(
+                    onPressed: clearHouse,
+                    icon: const Icon(
+                      Icons.replay_outlined,
+                      size: 40,
+                    ),
+                    label: const Text(
+                      'REINICIAR',
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
--- lib/widgets/tray_game.dart
+++ lib/widgets/tray_game.dart
@@ -3,6 +3,7 @@ import 'package:jogo_da_velha/widgets/type_house.dart';
 class TrayGame {
   var cardHouse = List<TypeHouse>.filled(9, TypeHouse.empty);
   var currentTurn = TypeHouse.X;
+  String winColor = '';
 
   //marcar a cardHouse
   void markHouse(int index) {
@@ -15,12 +16,31 @@ class TrayGame {
   }
 
   //verificar se o jogador ganhou
-  bool checkWin() {
-    for (int i = 0; i < 9; i++) {
-      if (cardHouse[i] == TypeHouse.empty) {
-        return false;
+  bool get checkWin {
+    List<String> winHouses = [
+      '012',
+      '345',
+      '678',
+      '036',
+      '147',
+      '258',
+      '048',
+      '246'
+    ];
+
+    for (var winElement in winHouses) {//ler por elemento
+      var char1 = int.parse(winElement[0]);
+      var char2 = int.parse(winElement[1]);
+      var char3 = int.parse(winElement[2]);
+
+      //ler por caractere
+      if (cardHouse[char1] == cardHouse[char2] &&
+          cardHouse[char3] == cardHouse[char2] &&
+          cardHouse[char2] == currentTurn) {
+        winColor = winElement;
+        return true;
       }
     }
-    return true;
+    return false;
   }
 }
